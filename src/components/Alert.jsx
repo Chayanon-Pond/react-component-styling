d4d1810578bd8faab800cb258f76152319ffc3c9
nantasit <nantasitnsboss@gmail.com>
2025-03-18 16:54:53 +0700

    fix(component):button and Alert to better(ans)

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,43 +3,85 @@
 import {css} from '@emotion/react'
 import { Frown, CircleAlert, TriangleAlert,CircleCheckBig } from 'lucide-react';
 
-const SizeIcon = 30;
-const AlertType= {
-    AlertError:     {text: "a error",
-                    bgColor: "#ffb3c6",
-                    icon:<Frown size={SizeIcon} color="red"/>},
-    AlertWarning: 
-                    { text: "a warning",
-                     bgColor:"rgb(255, 220, 199)",
-                     icon: <TriangleAlert size={SizeIcon} color="rgb(251, 98, 10)"/>},     
-    AlertInfo: 
-                     {text: "an alert",
-                     bgColor:"rgb(255, 240, 189)",
-                     icon: <CircleAlert size={SizeIcon} color="rgb(255, 165, 10)" />},  
-    AlertSuccess: 
-                     {text: "a success",
-                     bgColor:"rgb(189, 255, 191)",
-                     icon:<CircleCheckBig size={SizeIcon} color="rgb(7, 185, 13)" />}  
-}
-function Alert({type}) {
-    return(
-    <div 
-    css={css` 
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap:20px;
-        background-color: ${AlertType[type].bgColor};
-        padding: 8px;  
-        padding-left: 30px;
-        border-radius: 8px;
-        text-align: left;
-        ` }
-    >
-    {AlertType[type].icon}
-    <h2>This is {AlertType[type].text} alert box</h2>
-    </div>
-    )
+// const SizeIcon = 30;
+// const AlertType= {
+//     AlertError:     {text: "a error",
+//                     bgColor: "#ffb3c6",
+//                     icon:<Frown size={SizeIcon} color="red"/>},
+//     AlertWarning: 
+//                     { text: "a warning",
+//                      bgColor:"rgb(255, 220, 199)",
+//                      icon: <TriangleAlert size={SizeIcon} color="rgb(251, 98, 10)"/>},     
+//     AlertInfo: 
+//                      {text: "an alert",
+//                      bgColor:"rgb(255, 240, 189)",
+//                      icon: <CircleAlert size={SizeIcon} color="rgb(255, 165, 10)" />},  
+//     AlertSuccess: 
+//                      {text: "a success",
+//                      bgColor:"rgb(189, 255, 191)",
+//                      icon:<CircleCheckBig size={SizeIcon} color="rgb(7, 185, 13)" />}  
+// }
+// function Alert({type}) {
+//     return(
+//     <div 
+//     css={css` 
+//         display: flex;
+//         flex-direction: row;
+//         align-items: center;
+//         gap:20px;
+//         background-color: ${AlertType[type].bgColor};
+//         padding: 8px;  
+//         padding-left: 30px;
+//         border-radius: 8px;
+//         text-align: left;
+//         ` }
+//     >
+//     {AlertType[type].icon}
+//     <h2>This is {AlertType[type].text} alert box</h2>
+//     </div>
+//     )
+//     }
+
+
+//แบบเฉลย //กับมาแก้อยากกว่าด้านบน
+function Alert(prop) {
+    let bgColor = "rgb(255, 240, 189)";
+    let icon = <CircleAlert size={30} color="rgb(255, 165, 10)" />;
+    let text = "Info";
+    if(prop.type === "AlertError"){
+        bgColor = "#ffb3c6",
+        icon=<Frown size={30} color="red"/>
+        text='Error'
     }
+    else if(prop.type ==="AlertWarning"){
+        bgColor="rgb(255, 220, 199)",
+        icon= <TriangleAlert size={30} color="rgb(251, 98, 10)"/>
+        text='Warning'
+    }    
+    else if(prop.type === "AlertSuccess"){
+        bgColor="rgb(189, 255, 191)",
+        icon=<CircleCheckBig size={30} color="rgb(7, 185, 13)" />  
+        text="Success"
+    }
+    return(
+        <div 
+        css={css` 
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap:20px;
+            background-color: ${bgColor};
+            padding: 8px;  
+            padding-left: 30px;
+            border-radius: 8px;
+            text-align: left;
+            ` }
+        >
+        {icon}
+        <h2>This is {(text)} alert box</h2>
+        </div>
+        )
+        }
+
 
 export default Alert 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,51 +2,74 @@
 /**@jsxImportSource @emotion/react */
 import {css} from '@emotion/react'
 
-const Primary = css ` 
-width: 171.18959045410156px;
-height: 50px;
-border-radius: 4px;
-border:none;
-background-color: #074EE8;
-color: white;
-` 
-const Secondary = css ` 
-width: 171.18959045410156px;
-height: 50px;
-border-radius: 4px;
-border:none;
-padding-x: 16px;
-background-color: #07A4E8;
-color: white;
-` 
-const Error = css ` 
-width: 171.18959045410156px;
-height: 50px;
-border-radius: 4px;
-border:none;
-padding-x: 16px;
-background-color: #DE5753;
-color: white;
-` 
-const Success = css ` 
-width: 171.18959045410156px;
-height: 50px;
-border-radius: 4px;
-border:none;
-padding-x: 16px;
-background-color: #26B795;
-color: white;
-` 
+// const Primary = css ` 
+// width: 171.18959045410156px;
+// height: 50px;
+// border-radius: 4px;
+// border:none;
+// background-color: #074EE8;
+// color: white;
+// ` 
+// const Secondary = css ` 
+// width: 171.18959045410156px;
+// height: 50px;
+// border-radius: 4px;
+// border:none;
+// padding-x: 16px;
+// background-color: #07A4E8;
+// color: white;
+// ` 
+// const Error = css ` 
+// width: 171.18959045410156px;
+// height: 50px;
+// border-radius: 4px;
+// border:none;
+// padding-x: 16px;
+// background-color: #DE5753;
+// color: white;
+// ` 
+// const Success = css ` 
+// width: 171.18959045410156px;
+// height: 50px;
+// border-radius: 4px;
+// border:none;
+// padding-x: 16px;
+// background-color: #26B795;
+// color: white;
+// ` 
 
-function Button({type}) {
+// function Button({type}) {
+//     return(
+//         <button css={
+//             (type === "Primary")? Primary:
+//                 (type === "Secondary")?Secondary:
+//                     (type === "Success")?Success : Error
+//         }
+//         > Button </button>
+//     )
+// }
+
+แบบเฉลย
+function Button(prop) {
+    let bgColor ;
+    if(prop.type === "Primary" ){
+        bgColor ="#074EE8";
+    }else if(prop.type === "Secondary"){
+        bgColor = "#07A4E8";
+    }
     return(
-        <button css={
-            (type === "Primary")? Primary:
-                (type === "Secondary")?Secondary:
-                    (type === "Success")?Success : Error
-        }
-        > Button </button>
-    )
-}
+            <button css={ css ` 
+                width: 171.18959045410156px;
+                height: 50px;
+                border-radius: 4px;
+                border:none;
+                padding-x: 16px;
+                background-color: ${bgColor};
+                color: white;
+                `
+            }
+            > {prop.type} </button>
+        )
+    }
 
 export default Button
